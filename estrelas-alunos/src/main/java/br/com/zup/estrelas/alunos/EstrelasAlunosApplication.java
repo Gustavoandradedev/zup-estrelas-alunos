5ebd063f8c98753e4c0c7ed6c99a9df844438ed6
Gustavobjj <gustavoandrade15@hotmail.com>
2020-10-22 14:51:29 -0300

    Atualizei o projeto

---
+++ estrelas-alunos/src/main/java/br/com/zup/estrelas/alunos/EstrelasAlunosApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.estrelas.alunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstrelasAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstrelasAlunosApplication.class, args);
+	}
+
+}
